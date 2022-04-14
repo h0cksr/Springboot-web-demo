0074663bd3eeb3cb81524184bc2e50135a4a96d5
h0cksr <81439858+h0cksr@users.noreply.github.com>
2022-04-14 17:43:31 +0800

    Add files via upload

---
+++ src/main/java/com/test/WebController.java
@@ -0,0 +1,17 @@
+package com.test;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//注意,如果这里使用的是@RestController那么就不会跳转到/webapp下指定的test.jsp而是直接返回字符串test.jsp到响应页面
+//@RestController
+@Controller
+public class WebController {
+    @RequestMapping("/teststring")
+    public String teststring(){
+        return "hell0";
+    }
+    @RequestMapping("/testjsp")
+    public String testjsp(){
+        return "test.jsp";
+    }
+}
